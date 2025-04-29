9151b78acd18b1f0c0c4b20d3541b61bb0fd1b2a
ZhangSong <youremail@qq.com>
2025-04-29 19:26:41 +0800

    202504291

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  },
+  globals: {
+    Cesium: "readonly", // 声明 Cesium 为全局变量
+    // Highcharts: "readonly", // 声明 Highcharts 为全局变量
+  }
+}
